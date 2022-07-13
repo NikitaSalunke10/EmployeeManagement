6fac9eda30f04909af6b40cc34cf2f62a12fdc17
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-13 20:41:13 +0530

    UC-2 DailyWage

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,17 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Management");
-int IS_PART_TIME = 1;
-int IS_FULL_TIME = 2;
+int IS_FULL_TIME = 1;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
-int empCheck = random.Next(0, 3);
-if (empCheck == IS_PART_TIME)
-{
-    empHrs = 4;
-}
-else if (empCheck == IS_FULL_TIME)
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
 {
     empHrs = 8;
 }
