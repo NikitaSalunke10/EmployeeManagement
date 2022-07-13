3166209392ca1c038f9556efbb56cb175ef792ca
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-13 22:21:36 +0530

    UC-5 empwage for month

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,23 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Employee Management");
-const int IS_PART_TIME = 1;
+﻿const int IS_PART_TIME = 1;
 const int IS_FULL_TIME = 2;
+const int NUM_OF_WORKING_DAYS = 2;
 const int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empWage = 0;
+int totalEmpWage = 0;
 Random random = new Random();
-int empCheck = random.Next(0, 2);
-switch(empCheck)
+for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
 {
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-    default:
-        empHrs = 0;
-        break;
+    int empCheck = random.Next(0, 3);
+    switch (empCheck)
+    {
+        case IS_PART_TIME:
+            empHrs = 4;
+            break;
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
+            break;
+    }
+    empWage = empHrs * EMP_RATE_PER_HOUR;
+    totalEmpWage += empWage;
+    Console.WriteLine("Emp Wage: " + empWage);
 }
-empWage = empHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("Emp Wage: " + empWage);
+Console.WriteLine("Total Emp Wage: " + totalEmpWage);
