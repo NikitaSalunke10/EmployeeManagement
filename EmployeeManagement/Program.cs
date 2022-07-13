8b51a0000cba944512e5d4f38a9350c3f9928914
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-13 20:40:38 +0530

    UC-3 EmpWage Part-time

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Management");
-int IS_FULL_TIME = 1;
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
-int empCheck= random.Next(0,2);
-if(empCheck == IS_FULL_TIME)
+int empCheck = random.Next(0, 3);
+if (empCheck == IS_PART_TIME)
+{
+    empHrs = 4;
+}
+else if (empCheck == IS_FULL_TIME)
 {
     empHrs = 8;
 }
@@ -15,4 +20,4 @@ else
     empHrs = 0;
 }
 empWage = empHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("Emp Wage: " +empWage);
+Console.WriteLine("Emp Wage: " + empWage);
