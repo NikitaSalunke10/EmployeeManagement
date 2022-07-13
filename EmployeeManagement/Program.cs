ab35f7e9c95222abf388d28a918cfd224b75d592
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-13 22:30:10 +0530

    UC-6 empwage total working hrs per month

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -2,12 +2,14 @@
 const int IS_FULL_TIME = 2;
 const int NUM_OF_WORKING_DAYS = 2;
 const int EMP_RATE_PER_HOUR = 20;
+const int MAX_HRS_IN_MONTH = 10;
 int empHrs = 0;
-int empWage = 0;
-int totalEmpWage = 0;
-Random random = new Random();
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+int totalEmpHrs = 0;
+int totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
+    totalWorkingDays++;
+    Random random = new Random();
     int empCheck = random.Next(0, 3);
     switch (empCheck)
     {
@@ -21,8 +23,8 @@ for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             empHrs = 0;
             break;
     }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmpWage += empWage;
-    Console.WriteLine("Emp Wage: " + empWage);
+    totalEmpHrs +=empHrs;
+    Console.WriteLine("Day: " + totalWorkingDays + " Emp Hrs: " +empHrs);
 }
+int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Total Emp Wage: " + totalEmpWage);
