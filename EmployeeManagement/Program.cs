71624df59344d6a6d1c214d2e7ea405be67c549c
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-14 23:15:39 +0530

    UC-9 Total Wage for each Company

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,33 +1,8 @@
-﻿const int IS_PART_TIME = 1;
-const int IS_FULL_TIME = 2;
-static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-{
-    int empHrs = 0;
-    int totalEmpHrs = 0;
-    int totalWorkingDays = 0;
-    while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-    {
-        totalWorkingDays++;
-        Random random = new Random();
-        int empCheck = random.Next(0, 3);
-        switch (empCheck)
-        {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
-        }
-        totalEmpHrs += empHrs;
-        Console.WriteLine("Day#: " + totalWorkingDays + " Emp Hrs: " + empHrs);
-    }
-    int totalEmpWage = totalEmpHrs * empRatePerHour;
-    Console.WriteLine("Total Emp Wage for company: " + company + " is :" + totalEmpWage);
-    return totalEmpWage;
-}
-computeEmpWage("Dmart", 20, 2, 10);
-computeEmpWage("Reliance", 10, 4, 20);
+﻿using EmployeeManagement;
+
+EmpWageBuilderObject dMart = new EmpWageBuilderObject("Dmart", 20, 2, 10);
+EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+dMart.computeEmpWage();
+Console.WriteLine(dMart.toString());
+reliance.computeEmpWage();
+Console.WriteLine(reliance.toString());
